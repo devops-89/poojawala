71ce39e4e4d21f29f6235e17ce272b8e7406e6c2
yashwinDigixito <yashwinsharma16@gmail.com>
2026-09-11 17:53:10 +0530

    add new backend url

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
   async rewrites() {
     const AUTH_SERVER = process.env.NEXT_SERVER_URL;
     const USER_SERVER = process.env.NEXT_SERVER_URL;
-    const SOCKET_URL = process.env.NEXT_SOCKET_URL;
+    const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
     const PAYMENT_SERVER = process.env.NEXT_SERVER_URL;
 
     return [
