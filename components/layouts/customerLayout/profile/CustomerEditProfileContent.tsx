9c6e520e71deba211ff0d8185561cb38231a9c8f
yashwinDigixito <yashwinsharma16@gmail.com>
2026-09-11 15:27:36 +0530

    fix edit page

--- components/layouts/customerLayout/profile/CustomerEditProfileContent.tsx
+++ components/layouts/customerLayout/profile/CustomerEditProfileContent.tsx
@@ -545,7 +545,6 @@ function CustomerEditProfileContentInner() {
               </Box>
             )}
 
-            {/* ADDRESSES TAB */}
             {activeTab === "location" && (
               <Box>
                 <Box
